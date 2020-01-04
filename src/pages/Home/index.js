5ab261764724746330351240da58511e34eefba3
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-01-04 00:37:57 -0200

    feat: create home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.scss';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h3>React Template</h3>
+      <small>
+        <a href="https://github.com/melquisedecfelipe">github.com/melquisedecfelipe</a>
+      </small>
+    </div>
+  );
+}
