88d1db670f5a4720f52f937b1be8ffb8b01e00a3
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-01-04 12:28:56 -0200

    feat: lint app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 
-import history from './services/history';
-
 import Routes from './routes';
+import history from './services/history';
 
 import './App.scss';
 
