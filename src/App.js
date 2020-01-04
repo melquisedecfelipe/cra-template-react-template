0c2bd004889f499e75a3e7c698a8dec4f5c3d206
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-01-04 00:36:22 -0200

    feat: update app and add routes config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router } from 'react-router-dom';
 
-function App() {
+import history from './services/history';
+
+import Routes from './routes';
+
+import './App.scss';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <Routes />
+    </Router>
   );
 }
-
-export default App;
