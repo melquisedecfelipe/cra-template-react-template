a3504068fe036012f0eb0fc359a93d0ae3302fe9
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-01-07 16:26:24 -0200

    style: add skeleton loader in item

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MdArrowForward } from 'react-icons/md';
 
 import './styles.scss';
 
 export default function Item({ title, description, link }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
     <div className="item">
       <div>
-        <h3>{title}</h3>
-        <p>{description}</p>
+        <h3 className={loading ? 'loading title' : ''}>{loading ? '' : title}</h3>
+        <p className={loading ? 'loading description' : ''}>{loading ? '' : description}</p>
       </div>
-      <a href={link}>
-        <MdArrowForward />
+      <a className={loading ? 'loading link' : ''} href={link}>
+        {loading ? '' : <MdArrowForward />}
       </a>
     </div>
   );
