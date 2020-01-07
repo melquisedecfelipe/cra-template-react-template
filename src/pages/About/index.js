2beae43221e3148cb08d0b89b11f5ed3b60ed5ba
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-01-07 16:25:42 -0200

    feat: add function to get item and pass to props

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import Item from '../../components/Item';
 
+import api from '../../services/api';
+
 import './styles.scss';
 
 export default function About() {
+  const [item, setItem] = useState([1, 2, 3, 4, 5, 6]);
+
+  useEffect(() => {
+    async function getItems() {
+      const { data } = await api.get('5e133f493100005a8ad476cc');
+
+      setItem(data);
+    }
+
+    getItems();
+  }, []);
+
   return (
     <div className="about">
       <nav>
@@ -15,51 +29,9 @@ export default function About() {
         <h3>About</h3>
         <p>My bootstrap template to use in react</p>
         <div>
-          <Item
-            title="Axios"
-            description="Promise based HTTP client for the browser and node.js"
-            link="https://github.com/axios/axios"
-          />
-          <Item
-            title="Commitlint"
-            description="Lint commit messages"
-            link="https://github.com/conventional-changelog/commitlint"
-          />
-          <Item
-            title="Commitizen"
-            description="The commitizen command line utility"
-            link="https://github.com/commitizen/cz-cli"
-          />
-          <Item
-            title="Eslint"
-            description="Find and fix problems in your JavaScript code"
-            link="https://eslint.org/"
-          />
-          <Item
-            title="Eslint import helpers"
-            description="ESLint plugin to help enforce a configurable order for import statements"
-            link="https://github.com/Tibfib/eslint-plugin-import-helpers"
-          />
-          <Item
-            title="Husky"
-            description="Git hooks made easy"
-            link="https://github.com/typicode/husky"
-          />
-          <Item
-            title="React"
-            description="A JavaScript library for building user interfaces"
-            link="https://reactjs.org/"
-          />
-          <Item
-            title="React icons"
-            description="svg react icons of popular icon packs"
-            link="https://react-icons.netlify.com/"
-          />
-          <Item
-            title="React router"
-            description="Declarative routing for React"
-            link="https://reacttraining.com/react-router/"
-          />
+          {item.map(elem => (
+            <Item title={elem.title} description={elem.description} link={elem.link} />
+          ))}
         </div>
       </main>
     </div>
